031906a40b0ad5951dceb2e5bea612914056031f
eversonnatan <everson.natan@gmail.com>
2023-02-07 21:24:13 -0300

    add2

--- app/src/main/java/br/com/etecia/falecomigo/MainActivity.java
+++ app/src/main/java/br/com/etecia/falecomigo/MainActivity.java
@@ -2,14 +2,43 @@ package br.com.etecia.falecomigo;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-    ListView itens;
+    ListView idItens;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        idItens = findViewById(R.id.idItens);
+        MyAdapter adapter = new MyAdapter();
+        idItens.setAdapter(adapter);
+    }
+    public class MyAdapter extends BaseAdapter {
+
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
     }
 }
